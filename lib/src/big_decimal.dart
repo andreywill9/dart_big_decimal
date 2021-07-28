640c91cd80602d7e612e7c7eacd3a1e42ed0772e
andrey <andreywill9@gmail.com>
2021-07-28 08:25:47 -0300

    Implementation of Rounding Half Down

--- lib/src/big_decimal.dart
+++ lib/src/big_decimal.dart
@@ -1,9 +1,10 @@
 library big_decimal;
 
-import 'dart:math';
 
+import 'package:big_decimal/big_decimal.dart';
 import 'package:decimal/decimal.dart';
 
+import 'rouding.dart';
 import 'rounding_mode.dart';
 
 class BigDecimal {
@@ -19,42 +20,14 @@ class BigDecimal {
   
   static BigDecimal fromDouble(double valor) => BigDecimal._(Decimal.parse(valor.toString()));
 
-  static BigDecimal _roundHalfEven(Decimal value, int decimalPlaces) {
-    Decimal integralPart = value.truncate();
-    Decimal fractionalPart = _getFractionalPart(value);
-    int nextToLast = (fractionalPart * _powOfTen(decimalPlaces + 1) % Decimal.fromInt(10)).toInt();
-    Decimal fractionalMultiple = _powOfTen(decimalPlaces);
-    Decimal roundedFractionalPart = ((fractionalPart * fractionalMultiple).floor() / fractionalMultiple);
-    bool hasRemainder = _getFractionalPart(fractionalPart * _powOfTen(decimalPlaces + 1)) != Decimal.zero;
-    if (nextToLast < 5) return BigDecimal._(integralPart + roundedFractionalPart);
-    if (nextToLast > 5 || hasRemainder) roundedFractionalPart = _carryLatest(roundedFractionalPart, decimalPlaces);
-    else {
-      int lastConserved = (fractionalPart * _powOfTen(decimalPlaces) % Decimal.fromInt(10)).toInt();
-      if (lastConserved.isOdd) roundedFractionalPart = _carryLatest(roundedFractionalPart, decimalPlaces);
-    }
-    return BigDecimal._(integralPart + roundedFractionalPart);
-  }
-
-  static Decimal _powOfTen(int value) => Decimal.parse(pow(10, value).toString());
-
-  static Decimal _getFractionalPart(Decimal value) => value - value.truncate();
-
-  static Decimal _carryLatest(Decimal value, int decimalPlaces) {
-    int amount = value == Decimal.zero
-        ? decimalPlaces
-        : value.toString().length - 2;
-    Decimal offset = _powOfTen(-amount);
-    return value + offset;
-  }
-
-  BigDecimal multiply(BigDecimal secondValue, RoundingMode roundingMode, [int decimalPlaces = 2]) {
+  BigDecimal multiply(BigDecimal secondValue, RoundingMode roundingMode) {
     Decimal result = _value * secondValue._value;
-    return BigDecimal._(result).round(roundingMode, decimalPlaces);
+    return BigDecimal._(result);
   }
 
   BigDecimal divide(BigDecimal secondValue, RoundingMode roundingMode, [int decimalPlaces = 2]) {
     Decimal result = _value / secondValue._value;
-    return BigDecimal._(result).round(roundingMode, decimalPlaces);
+    return Rounding.round(result, roundingMode, decimalPlaces);
   }
 
   BigDecimal add(BigDecimal secondValue) {
@@ -82,25 +55,15 @@ class BigDecimal {
 
   BigDecimal min(BigDecimal secondValue) => secondValue._value < _value ? secondValue : this;
 
-  BigDecimal movePointToLeft(int places) => BigDecimal._(_value * (_powOfTen(places * -1)));
+  BigDecimal movePointToLeft(int places) => BigDecimal._(_value * (TEN.pow(places * -1, RoundingMode.ROUND_DOWN, 0))._value);
 
-  BigDecimal movePointToRight(int places) => BigDecimal._(_value * (_powOfTen(places)));
+  BigDecimal movePointToRight(int places) => BigDecimal._(_value * (TEN.pow(places, RoundingMode.ROUND_DOWN, 0))._value);
 
   BigDecimal abs() => BigDecimal._(_value.abs());
 
-  BigDecimal power(int exponent, RoundingMode roundingMode, [int decimalPlaces = 2]) {
+  BigDecimal pow(int exponent, RoundingMode roundingMode, [int decimalPlaces = 2]) {
     Decimal result = _value.pow(exponent);
-    return BigDecimal._(result).round(roundingMode, decimalPlaces);
-  }
-
-  BigDecimal round(RoundingMode mode, int decimalPlaces) {
-    switch (mode) {
-      case RoundingMode.HALF_EVEN:
-        return _roundHalfEven(_value, decimalPlaces);
-      default:
-      // NOT IMPLEMENTED YET
-        return this;
-    }
+    return Rounding.round(result, roundingMode, decimalPlaces);
   }
   
   int compareTo(BigDecimal secondValue) => _value.compareTo(secondValue._value);
---
+++ lib/src/rouding.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:decimal/decimal.dart';
+
+import 'big_decimal.dart';
+import 'rounding_mode.dart';
+
+abstract class Rounding {
+
+  static BigDecimal round(Decimal value, RoundingMode mode, [int decimalPlaces = 2]) {
+    switch (mode) {
+      case RoundingMode.HALF_EVEN:
+        return _roundHalfEven(value, decimalPlaces);
+      case RoundingMode.HALF_DOWN:
+        return _roundHalfDown(value, decimalPlaces);
+      default:
+      // NOT IMPLEMENTED YET
+        return BigDecimal.fromDouble(value.toDouble());
+    }
+  }
+
+  static BigDecimal _roundHalfEven(Decimal value, int decimalPlaces) {
+    Decimal integralPart = value.truncate();
+    Decimal fractionalPart = _getFractionalPart(value);
+    int nextToLast = _getNextToLast(fractionalPart, decimalPlaces);
+    Decimal fractionalMultiple = _powOfTen(decimalPlaces);
+    Decimal roundedFractionalPart = ((fractionalPart * fractionalMultiple).floor() / fractionalMultiple);
+    bool hasRemainder = _getFractionalPart(fractionalPart * _powOfTen(decimalPlaces + 1)) != Decimal.zero;
+    if (nextToLast < 5) return BigDecimal.fromDouble((integralPart + roundedFractionalPart).toDouble());
+    if (nextToLast > 5 || hasRemainder) roundedFractionalPart = _carryLatest(roundedFractionalPart, decimalPlaces);
+    else {
+      int lastConserved = (fractionalPart * _powOfTen(decimalPlaces) % Decimal.fromInt(10)).toInt();
+      if (lastConserved.isOdd) roundedFractionalPart = _carryLatest(roundedFractionalPart, decimalPlaces);
+    }
+    return BigDecimal.fromDouble((integralPart + roundedFractionalPart).toDouble());
+  }
+
+  static BigDecimal _roundHalfDown(Decimal value, int decimalPlaces) {
+    Decimal integralPart = value.truncate();
+    Decimal fractionalPart = _getFractionalPart(value);
+    int nextToLast = _getNextToLast(fractionalPart, decimalPlaces);
+    Decimal fractionalMultiple = _powOfTen(decimalPlaces);
+    Decimal roundedFractionalPart = ((fractionalPart * fractionalMultiple).floor() / fractionalMultiple);
+    if (nextToLast <= 5) return BigDecimal.fromDouble((integralPart + roundedFractionalPart).toDouble());
+    roundedFractionalPart = _carryLatest(roundedFractionalPart, decimalPlaces);
+    return BigDecimal.fromDouble((integralPart + roundedFractionalPart).toDouble());
+  }
+
+  static Decimal _powOfTen(int value) => Decimal.parse(pow(10, value).toString());
+
+  static Decimal _getFractionalPart(Decimal value) => value - value.truncate();
+
+  static int _getNextToLast(Decimal fractionalPart, int decimalPlaces) =>
+      (fractionalPart * _powOfTen(decimalPlaces + 1) % Decimal.fromInt(10)).toInt();
+
+  static Decimal _carryLatest(Decimal value, int decimalPlaces) {
+    int amount = value == Decimal.zero
+        ? decimalPlaces
+        : value.toString().length - 2;
+    Decimal offset = _powOfTen(-amount);
+    return value + offset;
+  }
+}
